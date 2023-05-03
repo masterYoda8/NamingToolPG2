0ded5a7375b54fa38a2f46ff3cc3b90be7e80a66
Florian <flo.remberger@gmail.com>
2023-05-03 21:51:03 +0200

    Add tests for some functions in utils.cpp

---
+++ test.sh
@@ -0,0 +1,3 @@
+g++ -std=c++20 -o test -lgtest -lgtest_main src/utils.cpp tests/test.cpp
+./test
+rm test
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,11 +1,22 @@
 #include "googletest/googlemock/include/gmock/gmock.h" 
-#include "../src/main.h"
+#include "../src/utils.h"
+#include <string>
 
-TEST(Exampletest, passes) {
-    ASSERT_THAT(test(), testing::Eq(10));
+using testing::Eq;
+
+TEST(getSubjectIndex, returnsIndexForValidSubjectInFilePath) {
+    std::string filePathWithPG2 = "blip/blap/blup/pg2/blip/blap/blup";    
+    std::string filePathWithDS = "blip/ds/test/d/a/x";
+    
+    initSubjectVector();
+    
+    ASSERT_THAT(getSubjectIndex(filePathWithPG2), Eq(1));
+    ASSERT_THAT(getSubjectIndex(filePathWithDS), Eq(0));
 }
 
-TEST(test, failes) {
-    ASSERT_THAT(test(), testing::Eq(11));
+TEST(extractPractiseNumber, returnsPractiseNumberString) {
+    std::string filePathWithPractiseNumber = "test/this/u49/seven/eight";
+
+    ASSERT_THAT(extractPractiseNumber(filePathWithPractiseNumber), Eq("49"));
 }
 
